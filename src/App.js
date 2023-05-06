01a912c64a6fa353deb4051998633a0feced708e
Seb <sunipan@gmail.com>
2023-05-05 18:34:21 -0700

    added tailwind,axios,created Attributes component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { useState } from "react";
+import "./App.css";
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts.js";
+import { Attributes } from "./components/Attributes";
 
 function App() {
-  const [num, setNum] = useState(0);
+  const [attributes, setAttributes] = useState(
+    ATTRIBUTE_LIST.reduce((acc, attribute) => ({ ...acc, [attribute]: 10 }), {})
+  );
+
+  const handleAttributeIncrement = (attribute) => {
+    setAttributes({ ...attributes, [attribute]: attributes[attribute] + 1 });
+  };
+  const handleAttributeDecrement = (attribute) => {
+    setAttributes({
+      ...attributes,
+      [attribute]:
+        attributes[attribute] - 1 < 0 ? 0 : attributes[attribute] - 1,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
       <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
+        <div className="grid grid-cols-3 gap-4">
+          <Attributes
+            attributes={attributes}
+            onIncrement={handleAttributeIncrement}
+            onDecrement={handleAttributeDecrement}
+          />
         </div>
       </section>
     </div>
---
+++ src/components/Attributes.jsx
@@ -0,0 +1,32 @@
+import { ATTRIBUTE_LIST } from "../consts";
+
+export const Attributes = ({ attributes, onIncrement, onDecrement }) => {
+  return (
+    <div className="flex flex-col">
+      <div className="text-center font-bold text-xl mb-2">Attributes</div>
+      <div className="grid gap-2 h-full">
+        {ATTRIBUTE_LIST.map((attribute) => (
+          <div key={attribute} className="grid grid-cols-3">
+            <div className="text-center col-span-2">
+              {attribute}: {attributes[attribute]}
+            </div>
+            <div className="flex justify-center items-center">
+              <button
+                className="text-center w-6 h-6 rounded-full mr-2 bg-white text-black"
+                onClick={() => onDecrement(attribute)}
+              >
+                -
+              </button>
+              <button
+                className="text-center w-6 h-6 bg-white text-black rounded-full"
+                onClick={() => onIncrement(attribute)}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
