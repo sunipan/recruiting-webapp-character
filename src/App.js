cf034d356fefa6cdff2f07c802efbef18177dd6b
Seb <sunipan@gmail.com>
2023-05-05 19:10:52 -0700

    added skills class with incrementing functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import "./App.css";
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts.js";
 import { Attributes } from "./components/Attributes";
 import { Classes } from "./components/Classes";
+import { Skills } from "./components/Skills";
+import { calculateModifier } from "./utils/utils";
 
 function App() {
+  /* State */
   const [attributes, setAttributes] = useState(
     ATTRIBUTE_LIST.reduce((acc, attribute) => ({ ...acc, [attribute]: 10 }), {})
   );
+  const [skills, setSkills] = useState(
+    SKILL_LIST.reduce(
+      (acc, { name, attributeModifier }) => ({
+        ...acc,
+        [name]: calculateModifier(attributes, attributeModifier),
+      }),
+      {}
+    )
+  );
 
+  /* Handlers */
   const handleAttributeIncrement = (attribute) => {
     setAttributes({ ...attributes, [attribute]: attributes[attribute] + 1 });
   };
+
   const handleAttributeDecrement = (attribute) => {
     setAttributes({
       ...attributes,
@@ -20,19 +34,41 @@ function App() {
     });
   };
 
+  const handleSkillIncrement = (skill) => {
+    const totalPoints = Object.values(skills).reduce((a, b) => a + b, 0);
+    setSkills((prev) => {
+      if (totalPoints >= 10 + 4 * calculateModifier(attributes, "Intelligence"))
+        return prev;
+      return { ...prev, [skill]: prev[skill] + 1 };
+    });
+  };
+
+  const handleSkillDecrement = (skill) => {
+    setSkills((prev) => ({
+      ...prev,
+      [skill]: prev[skill] - 1 < 0 ? 0 : prev[skill] - 1,
+    }));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
       <section className="App-section">
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-4 gap-4">
           <Attributes
             attributes={attributes}
             onIncrement={handleAttributeIncrement}
             onDecrement={handleAttributeDecrement}
           />
           <Classes classes={CLASS_LIST} attributes={attributes} />
+          <Skills
+            skills={skills}
+            attributes={attributes}
+            onIncrement={handleSkillIncrement}
+            onDecrement={handleSkillDecrement}
+          />
         </div>
       </section>
     </div>
---
+++ src/components/Skills.jsx
@@ -0,0 +1,47 @@
+import { SKILL_LIST } from "../consts";
+import { calculateModifier } from "../utils/utils";
+
+export const Skills = ({ skills, attributes, onIncrement, onDecrement }) => {
+  return (
+    <div className="flex flex-col col-span-2">
+      <div className="text-center font-bold text-xl mb-2">Skills</div>
+      <div>
+        Total Points: {10 + 4 * calculateModifier(attributes, "Intelligence")}
+      </div>
+      <div className="grid h-full">
+        {SKILL_LIST.map((skill) => {
+          return (
+            <div className="grid grid-cols-5" key={skill.name}>
+              <div className="col-span-2">
+                {skill.name}: {skills[skill.name]}
+              </div>
+              <div>
+                Mod:&nbsp;
+                {calculateModifier(attributes, skill.attributeModifier)}
+              </div>
+              <div>
+                Total:{" "}
+                {skills[skill.name] +
+                  calculateModifier(attributes, skill.attributeModifier)}
+              </div>
+              <div>
+                <button
+                  className="mr-1 w-6 h-6 rounded-full bg-white text-black"
+                  onClick={() => onIncrement(skill.name)}
+                >
+                  +
+                </button>
+                <button
+                  onClick={() => onDecrement(skill.name)}
+                  className="w-6 h-6 rounded-full bg-white text-black"
+                >
+                  -
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
